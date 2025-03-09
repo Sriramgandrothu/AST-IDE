e1bce0d757bf7713a11d42f6afd0abfb6525d41d
SriramGandrothu <sriramgandrothu@gmail.com>
2025-03-09 20:49:09 +0530

    Removed Navigate from AllRoutes

--- client/src/AllRoutes.tsx
+++ client/src/AllRoutes.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import Loader from "./components/Loader/Loader";
